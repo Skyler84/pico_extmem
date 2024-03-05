33b907e61bd710bcf500c1b67aa2c063282112ab
Skyler Mansfield <skyler.mansfield.21@gmail.com>
2024-03-05 14:45:44 +0000

    Move main executable to examples dir.

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,11 +1,9 @@
-#include <stdio.h>
+#include <cstdio>
 #include "pico/stdio.h"
-// #include "pico/stlib.h"
 #include "pico/time.h"
 #include "hardware/exception.h"
 #include "hardware/watchdog.h"
 #include "pico/bootrom.h"
-#include <array>
 
 #include "spiram.hpp"
 #include "extmem_mapper.hpp"
@@ -22,6 +20,7 @@ int main(){
   SpiRam extmem{19, 16, 18, 3};
   ExtmemMapper::init(&extmem, 0x0300'0000);
 
+  // Allow the usb serial time to connect
   for (int i = 10; i; --i) {
     printf("waiting %ds\n", i);
     sleep_ms(1000);
@@ -30,14 +29,18 @@ int main(){
 
 
   printf("hardfaulting!\n");
+  //set some registers for us to look at
   asm volatile (
     "mov r0, #0\n\t"
-    "mov r1, #1");
-  (*(volatile int*)(0x30000000)) = 0x5678;
-  sleep_ms(100);
+    "mov r1, #1\n\t"
+    "str %0, [%1, #0]"
+    :: "r" (0x5678), "r" (0x3000'0000));
+  // (*(volatile int*)(0x3000'0000)) = 0x5678;
+
   printf("Hardfault handled\n");
   printf("Value at addr 0: %08lx\n", extmem.read_dword(0));
-  extmem.write_dword(0, 0x5678);
-  printf("Value at addr 0: %08lx\n", extmem.read_dword(0));
+  extmem.write_dword(4, 0x1234);
+  printf("Reading 0x30000000: %08lx\n", (*(volatile int*)(0x3000'0000)));
+  printf("Reading 0x30000004: %08lx\n", (*(volatile int*)(0x3000'0004)));
   while(true);
 }
