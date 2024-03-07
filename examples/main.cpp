d81e90f7007e9adb5b214e9c1c0e452879f9c5c6
Skyler Mansfield <skyler.mansfield.21@gmail.com>
2024-03-07 17:20:31 +0000

    Example: make it more of an example, less profile
    removed timing from example, this will be moved to tests/profiling app

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,28 +1,12 @@
 #include <cstdio>
 #include "pico/stdio.h"
 #include "pico/time.h"
-#include "hardware/exception.h"
 #include "hardware/watchdog.h"
 #include "pico/bootrom.h"
 
 #include "spiram.hpp"
 #include "cached_memory.hpp"
 #include "extmem_mapper.hpp"
-
-void time_hardfaults(){
-  uint64_t start = time_us_64();
-  uint32_t niters = 5'000;
-
-  for (uint32_t i = 0; i < niters; i++) {
-    (*(volatile int*)(0x3000'0000));
-    (*(volatile int*)(0x3000'0000 + i*4)) = i;
-    watchdog_update();
-  }
-
-  uint64_t end = time_us_64();
-  uint64_t timedelta = end-start;
-  printf("Time taken for %d hardfaults: %lldus\n%lld hardfaults/second", niters, timedelta, uint64_t((uint64_t(1'000'000)*uint64_t(niters))/timedelta)*2);
-}
 
 int main(){
   stdio_init_all();
@@ -31,38 +15,37 @@ int main(){
     reset_usb_boot(0,0);
   }
 
-  watchdog_enable(2000, 1);
+  watchdog_enable(4000, 1);
 
   SpiRam extmem{19, 16, 18, 3};
-  CachedMemory cache{&extmem};
+  Cached_32_32 cache(&extmem);
   ExtmemMapper::init(&cache, 0x3000'0000);
 
-  // Allow the usb serial time to connect
-  for (int i = 10; i; --i) {
-    printf("waiting %ds\n", i);
+  while(!stdio_usb_connected()){
     sleep_ms(1000);
+    printf("waiting\n");
     watchdog_update();
   }
-
-
-  printf("hardfaulting!\n");
-  //set some registers for us to look at
-  asm volatile (
-    "mov r0, #0\n\t"
-    "mov r1, #1\n\t"
-    "str %0, [%1, #0]"
-    :: "r" (0x5678), "r" (0x3000'0000));
-  // (*(volatile int*)(0x3000'0000)) = 0x5678;
-
+  sleep_ms(1000);
+  
+  uintptr_t base_addr = 0x3000'0000;
+  uintptr_t addr = 0x3000'0000;
+  uint32_t value = 0x1234'5678;
+  printf("Writing %08x to %p!\n", value, addr);
+  *((volatile uint32_t*)(addr)) = value;
   printf("Hardfault handled\n");
-  printf("Value at addr 0: %08lx\n", cache.read_dword(0));
-  cache.write_dword(4, 0x1234);
-  printf("Reading 0x30000000: %08lx\n", (*(volatile int*)(0x3000'0000)));
-  printf("Reading 0x30000004: %08lx\n", (*(volatile int*)(0x3000'0004)));
+  printf("Value at addr %p: %08x\n", addr-base_addr, cache.read_dword(addr-base_addr));
 
-  time_hardfaults();
-  for (int i = 5; i; --i) {
-    printf("Cooling down, %ds\n", i);
-  }
+  uint32_t read_addr = 0x3000'8020;
+  uint32_t read_test_value = 0xc0de'cafe;
+  uint32_t read_value;
+  printf("Setting addr %p: %08x\n", read_addr-base_addr, read_test_value);
+  cache.write_dword(read_addr-base_addr, read_test_value);
+  printf("Reading from %p!\n", read_addr);
+  read_value = *((volatile uint32_t*)(read_addr));
+  printf("Hardfault handled\n");
+  printf("Read %08x from %p\n", read_value, read_addr);
+
+  printf("Done, resetting\n");
   while(true);
 }
